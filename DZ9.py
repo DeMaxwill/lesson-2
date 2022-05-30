774109defc8a74f5c3d3fda1ddcb8a01ab5c873e
DeMaxwill <demaxwill@gmail.com>
2022-05-31 00:20:30 +0300

    Changed

--- DZ9.py
+++ DZ9.py
@@ -1,6 +1,12 @@
-a = input().split()
-m = 0
-for i in range(1, len(a)-1):
-    if int(a[i-1]) < int(a[i]) and int(a[i]) > int(a[i+1]):
-        m += 1
-print(m)
+numberList = [1, 9, 7, 4, 5, 20, 5, 6, 1]
+index = 1  # счетчик прохода по списку, начнем со второго элемента (с индексом 1)
+sosedi = 0  # счетчик "больших соседей"
+
+# цикл прохода по списку, с1
+while index < len(numberList) - 1:
+    if numberList[index - 1] < numberList[index] > numberList[index + 1]:
+        sosedi += 1
+        index += 2
+    else:
+        index += 1
+print(sosedi)
