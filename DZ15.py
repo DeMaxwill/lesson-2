57e46672f1e1a05158826b8449ea1cd369ebece4
DeMaxwill <demaxwill@gmail.com>
2022-06-05 22:18:36 +0300

    New files

---
+++ DZ15.py
@@ -0,0 +1,21 @@
+a_dict = '2', '4', '6', '8'
+b_dict = '3', '5', '7', '9'
+c_dict = '+', '-', '*', '/'
+
+a=a_dict
+b=b_dict
+c=c_dict
+
+
+def arithmetic(a, b, c):
+    if c == "+":
+        return a + b
+    elif c == "-":
+        return a - b
+    elif c == "*":
+        return a * b
+    elif c == "/":
+        return a / b
+    else:
+        print("Неизвестная операция ")
+arithmetic(a, b, c)
