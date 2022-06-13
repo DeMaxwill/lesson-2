44291f5fbf1fd036720c3b99d6270e5d95513b42
DeMaxwill <demaxwill@gmail.com>
2022-06-13 07:42:26 +0300

    Fix

--- DZ16.py
+++ DZ16.py
@@ -1,13 +1,40 @@
-def login():
-  for t in reversed(range(4)):
-    ll= input("Введите логин: ")
-    p = input('Введите пароль: ')
-    if p == 'indahouse':
-       if ll == 'Max':
-        print('Вы в системе!')
-        break
-    if t > 0:
-        print('Осталось попыток', t)
-  else:
-    print('Неправельное Имя или Пароль')
-login()
+logs = {"Max": "indahouse"}
+
+def decorator (func):
+    def wrapper(name, pwd):
+        if check_password(name, pwd):
+            return func(name, pwd)
+        else:
+            return False
+
+    return wrapper
+
+@decorator
+def login(name: str, pwd: str) ->bool:
+    return True
+
+
+def check_password(name: str, pwd: str) -> bool:
+    return logs.get(name)==pwd
+
+
+if __name__ == '__main__':
+
+    counter = 3
+    while counter >= 1:
+
+        print(f"У вас осталось {counter} попыток!")
+
+        username = input("Логин: ")
+        password = input("Пароль: ")
+
+        log = login(username, password)
+
+        if log is True:
+            print("Вы в системе!")
+            break
+        else:
+            print("Неправильный Логин или Пароль!")
+            counter -= 1
+    else:
+        print("Попыток больше нет.. Прощайте")
