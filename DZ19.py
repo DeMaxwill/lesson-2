2d53a6550fe854c4643db73863d29a2b059e9c52
DeMaxwill <demaxwill@gmail.com>
2022-06-24 16:49:33 +0300

    Update

--- DZ19.py
+++ DZ19.py
@@ -1,9 +1,7 @@
-def flatten(s):
-    if s == []:
-        return s
-    if isinstance(s[0], list):
-        return(flatten(s[0]) + flatten(s[1:]))
-    return(s[:1] + flatten(s[1:]))
-s = [1,2,3, [1,2,[1,2], [4, [5,[6, [4]]], [3, 1]]]]
-print(flatten(s))
-print(sum(flatten(s)))
+
+lst = [1, 2, 3, [1, 2, [1, 2], [4, [5, [6, [4]]], [3, 1]]]]
+flatten = lambda lst: isinstance(lst, (int)) and [lst] or sum(map(flatten, lst), [])
+
+flat_list = flatten(lst)
+print(flat_list)
+print(sum(flat_list))
