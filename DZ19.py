b2f44d23ecf20b3d0dcc058f3c855f7df23ce756
DeMaxwill <demaxwill@gmail.com>
2022-06-25 10:45:13 +0300

    Update

--- DZ19.py
+++ DZ19.py
@@ -3,5 +3,7 @@ lst = [1, 2, 3, [1, 2, [1, 2], [4, [5, [6, [4]]], [3, 1]]]]
 flatten = lambda lst: isinstance(lst, (int)) and [lst] or sum(map(flatten, lst), [])
 
 flat_list = flatten(lst)
-print(flat_list)
-print(sum(flat_list))
+
+fl = [flat_list[i:i + 2] for i in range(0, len(flat_list), 2)]
+print(len(fl) + len(flat_list))
+
