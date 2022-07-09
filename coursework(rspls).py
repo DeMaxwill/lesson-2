96af5c6c355aa7a68b0200c4cfa3bcfc0962c199
DeMaxwill <demaxwill@gmail.com>
2022-07-09 10:27:49 +0300

    Update

--- coursework(rspls).py
+++ coursework(rspls).py
@@ -1,132 +1,51 @@
-import random
+from random import choice
 
-print("----------------------------------------------------")
 print("-------Камень, Ножницы, Бумага, Ящерица, Спок-------")
 print("------------!Добро пожаловать в Игру!---------------")
-print("----------------Игра в три раунда.------------------")
 print("-------------------Правила--------------------------")
 f = open("rules.txt")
 data = f.read()
 print(data)
 f.close()
-print("[rock] - Камень ")
-print("[scissors] - Ножницы ")
-print("[paper] - Бумага ")
-print("[lizard] - Ящерица")
-print("[spock] - Спок")
 
-count = [0, 0]
 
-print("-----------------------------------------------------")
-print("------------------Игра Началась----------------------")
+rspls = {"камень": ["ножницы", "ящерица"],
+         "ножницы": ["бумага", "ящерица"],
+         "бумага": ["камень", "спок"],
+         "спок": ["ножницы", "камень"],
+         "ящерица": ["бумага", "спок"]}
 
 
 def main():
-    while True:
-        for i in range(3):
-            print("-------РАУНД №" + str(i + 1) + "--")
-            rpsls = "rock", "scissors", "paper", "lizard", "spock"
-            comp_select = random.choice(rpsls)
-            while True:
-                player_select = input("Ваш выбор?\n>>> ").lower()
 
-                if (player_select == "rock") or (player_select == "scissors") or (player_select == "paper") \
-                        or (player_select == "lizard") or (player_select == "spock"):
-                    break
-                else:
-                    print(f"Invalid input " + '"' + player_select + '"')
-            print("Игрок: " + player_select)
-            print("Компьютер: " + comp_select)
-            if player_select == comp_select:
-                print("<<Ничья>>")
-            elif player_select == "rock" and comp_select == "scissors":
-                print("$$$ Вы Выиграли $$$")
-                count[0] += 1
-            elif player_select == "rock" and comp_select == "lizard":
-                print("$$$ Вы Выиграли $$$")
-                count[0] += 1
-            elif player_select == "rock" and comp_select == "paper":
-                print("ХХХ Выиграл Компьютер ХХХ")
-                count[1] += 1
-            elif player_select == "rock" and comp_select == "spock":
-                print("ХХХ Выиграл Компьютер ХХХ")
-                count[1] += 1
-            elif player_select == "rock" and comp_select == "paper":
-                print("ХХХ Выиграл Компьютер ХХХ")
-                count[1] += 1
-            elif player_select == "paper" and comp_select == "rock":
-                print("$$$ Вы Выиграли $$$")
-                count[0] += 1
-            elif player_select == "paper" and comp_select == "spock":
-                print("$$$ Вы Выиграли $$$")
-                count[0] += 1
-            elif player_select == "paper" and comp_select == "scissors":
-                print("ХХХ Выиграл Компьютер ХХХ")
-                count[1] += 1
-            elif player_select == "paper" and comp_select == "lizard":
-                print("ХХХ Выиграл Компьютер ХХХ")
-                count[1] += 1
-            elif player_select == "scissors" and comp_select == "paper":
-                print("$$$ Вы Выиграли $$$")
-                count[0] += 1
-            elif player_select == "scissors" and comp_select == "lizard":
-                print("$$$ Вы Выиграли $$$")
-                count[0] += 1
-            elif player_select == "scissors" and comp_select == "rock":
-                print("ХХХ Выиграл Компьютер ХХХ")
-                count[1] += 1
-            elif player_select == "scissors" and comp_select == "spock":
-                print("ХХХ Выиграл Компьютер ХХХ")
-                count[1] += 1
-            elif player_select == "lizard" and comp_select == "paper":
-                print("$$$ Вы Выиграли $$$")
-                count[0] += 1
-            elif player_select == "lizard" and comp_select == "spock":
-                print("$$$ Вы Выиграли $$$")
-                count[0] += 1
-            elif player_select == "lizard" and comp_select == "rock":
-                print("ХХХ Выиграл Компьютер ХХХ")
-                count[1] += 1
-            elif player_select == "lizard" and comp_select == "scissors":
-                print("ХХХ Выиграл Компьютер ХХХ")
-                count[1] += 1
-            elif player_select == "spock" and comp_select == "rock":
-                print("$$$ Вы Выиграли $$$")
-                count[0] += 1
-            elif player_select == "spock" and comp_select == "scissors":
-                print("$$$ Вы Выиграли $$$")
-                count[0] += 1
-            elif player_select == "spock" and comp_select == "paper":
-                print("ХХХ Выиграл Компьютер ХХХ")
-                count[1] += 1
-            elif player_select == "spock" and comp_select == "lizard":
-                print("ХХХ Выиграл Компьютер ХХХ")
-                count[1] += 1
-        print("------------------------------------")
-        print("----------Финальные Очки------------")
-        if count[0] > count[1]:
-            print("***Поздравляем! Вы Победитель!***")
-            print(f"Игрок - {count[0]}, Компьютер - {count[1]}")
-            print("------------------------------------")
-            ask_user()
-        elif count[0] < count[1]:
-            print("***Вы Проиграли =( Компьютер Победил!***")
-            print(f"Игрок - {count[0]}, Компьютер - {count[1]}")
-            print("------------------------------------")
+    player_select = input("Ваш Выбор (камень, ножницы, бумага, ящерица или спок)?\n>>> ").lower()
+
+    if player_select not in rspls:
+        print(f"Invalid input " + '"' + player_select + '"')
+        main()
+    else:
+        comp_select = choice("камень/ножницы/бумага/ящерица/спок".split("/"))
+        print("Игрок: " + player_select)
+        print("Компьютер: " + comp_select)
+
+        if player_select == comp_select:
+            print('Ничья!')
             ask_user()
         else:
-            print("!!!Ничья!!!")
-            print(f"Игрок - {count[0]}, Компьютер - {count[1]}")
-            print("------------------------------------")
-            ask_user()
+            if player_select in rspls[comp_select]:
+                print('Компьютер Выйграл!')
+                ask_user()
+            else:
+                print('Игрок Выйграл!')
+                ask_user()
 
 
 def ask_user():
-    check = str(input("Играем еще раз? да или нет\n>>> ")).lower().strip()
+    check = str(input("Продолжить? y/n\n>>> ")).lower().strip()
     try:
-        if check == 'да':
+        if check == 'y':
             return main()
-        elif check == 'нет':
+        elif check == 'n':
             exit()
         else:
             print(f"Invalid input " + '"' + check + '"')
